06c1b4bb94d506752a7e16dbbdf576d8c9518569
TheGeekMax <maximesanciaume@gmail.com>
2026-08-30 19:09:07 +0300

    fix mastermind

--- minigame/board/mastermind.lua
+++ minigame/board/mastermind.lua
@@ -37,7 +37,7 @@
 -- ============================================================
 
 local MONITORS_CONFIG_PATH = "monitors.cfg"
-local TEXT_SCALE = 0.5
+local TEXT_SCALE = 1
 local CODE_LENGTH = 4
 local MAX_GUESSES = 10
 
